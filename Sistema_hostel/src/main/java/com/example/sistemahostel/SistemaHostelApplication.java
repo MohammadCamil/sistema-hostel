ada546d35dee87e820d94b0d03afd3c0a62685d0
mohammadcamil <mohammadcamil@172.30.30.159>
2020-06-15 09:26:40 +0530

    First Commit

---
+++ Sistema_hostel/src/main/java/com/example/sistemahostel/SistemaHostelApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemahostel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaHostelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaHostelApplication.class, args);
+	}
+
+}
---
+++ Sistema_hostel/src/test/java/com/example/sistemahostel/SistemaHostelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistemahostel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaHostelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
